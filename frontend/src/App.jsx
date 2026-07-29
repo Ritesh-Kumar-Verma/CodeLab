170e1dc4fa56b22c7bb39d5f1eeb1a014ac1b7a1
Ritesh Kumar Verma <ritesh782002@gmail.com>
2026-07-29 19:49:22 +0530

    updated visual for frontend and added code running functionality to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CodeEditor from './components/CodeEditor'
 const App = () => {
   return (
-    <div className='bg-blue-500'>
+    <div className='bg-[#181818] w-full h-screen'>
       <CodeEditor/>
 
 
